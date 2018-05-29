0e75f3abd1665924b021c2bb9ce43bb6c16800a7
EduStrife <edustrife@gmail.com>
2018-05-29 17:47:14 +0200

    registro.html calculation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function () {
   "use strict";
 
+  var regalo=document.getElementById('regalo');
+
   document.addEventListener('DOMContentLoaded', function () {
     //console.log("empezamos");
     //Datos de usuario
@@ -17,8 +19,78 @@
     var calcular=document.getElementById('calcular');
     var errorDiv=document.getElementById('error');
     var botonRegistro=document.getElementById('btnRegistro');
-    var resultado=document.getElementById('lista_productos');
+    var lista_productos=document.getElementById('lista-productos');
+    var suma = document.getElementById('suma-total');
 
+    // Extras
+    var sagrada =document.getElementById('entrada_sagrada');
+    var guell = document.getElementById('entrada_guell');
+
+    //botonRegistro.disabled = true;
+
+    calcular.addEventListener('click', calcularPrecio);
+
+    dia_completo.addEventListener('blur', mostrarDias);
+    rutas.addEventListener('blur', mostrarDias);
+    traslados.addEventListener('blur', mostrarDias);
+
+
+    function  calcularPrecio(event) {
+      event.preventDefault();
+      //console.log(regalo.value);
+      if (regalo.value === ''){
+        alert("Escoge un regalo");
+        regalo.focus();
+      } else {
+        //console.log('regalo escogido: '+ regalo.value);
+        var serviciosDia = dia_completo.value, //TODO quizá añadir un parseInt
+          serviciosTraslados = traslados.value,
+          serviciosRutas = rutas.value,
+          entradasSagrada = sagrada.value,
+          entradasGuell = guell.value;
+
+        //sólo para debug
+        /*console.log("servicios dia: " + serviciosDia);
+        console.log("servicios traslado: " + serviciosTraslados);
+        console.log("servicios rutas: " + serviciosRutas);*/
+
+        var totalPagar = (serviciosDia * 200) + (serviciosTraslados * 30) + (serviciosRutas * 50) + ((entradasSagrada * 12) * .93) + (entradasGuell * 10);
+        //console.log("Total: " + totalPagar)
+
+        var listadoProductos = [];
+        if(serviciosDia >= 1) {
+          listadoProductos.push(serviciosDia + ' Dia completo');
+        }
+        if(serviciosTraslados >= 1) {
+          listadoProductos.push(serviciosTraslados + ' Traslados');
+        }
+        if(serviciosRutas >= 1) {
+          listadoProductos.push(serviciosRutas + ' Rutas');
+        }
+        if(entradasSagrada >= 1) {
+          listadoProductos.push(entradasSagrada + ' Entradas Sagrada Familia');
+        }
+        if(entradasGuell >= 1) {
+          listadoProductos.push(entradasGuell + ' Entradas Parc Guell');
+        }
+        //ahora imprimir la lista de pedidos en el html
+        lista_productos.style.display = "block";//con esto y el css para que no salga recuadro gris cuando no hay nada.
+        lista_productos.innerHTML = '';
+        for(var i = 0; i< listadoProductos.length; i++) {
+          lista_productos.innerHTML += listadoProductos[i] + '<br/>';//a cada vuelta lo saca
+        }
+
+        suma.innerHTML = "$ " + totalPagar.toFixed(2);//IMPORTANTíSIMO, recortar decimales con esto o la pasarela de pago da error si hay más decimales
+
+        /*botonRegistro.disabled = false;
+        document.getElementById('total_pedido').value = totalPagar;*/
+
+
+
+
+
+      }
+    }
 
   }); //DOM CONTENT LOADED
 })();
